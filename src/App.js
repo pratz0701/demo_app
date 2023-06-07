9ccf3caec3fffe3d8e502067c71af09f107c40c9
pratz0701 <pratishthasingh0701@gmail.com>
2023-06-07 13:35:05 +0530

    file upload added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import FileUpload from './FileUpload';
 
 function App() {
   return (
-<hi> hey</hi>
+   <FileUpload/>
   );
 }
 
---
+++ src/FileUpload/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const FileUpload = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    // Handle the file upload logic here, e.g., making an HTTP request to the server
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+      setSelectedFile(null);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+};
+
+export default FileUpload;
